883d8a487d04f8c838a0db7960420868aecbcdc1
bartoszbereski <101666686+bartoszbereski@users.noreply.github.com>
2023-05-10 16:24:31 +0200

    Create algorithm.py

---
+++ algorithm.py
@@ -0,0 +1,54 @@
+from copy import deepcopy   # it is copying object itself instead of reference to object
+import pygame
+
+RED = (178,34,34)
+WHITE = (220,220,220)
+
+"position - where we are in right now; depth - how far the tree is going to be; "
+"max_player - describes if we are minimazing or maximizing the value; max_player = False we are trying to minimize"
+"game - game object"
+
+
+def minimax(position, depth, max_player, game):
+    if depth == 0 or position.winner() is not None:
+        return position.evaluate(), position
+
+    if max_player:
+        max_eval = float('-inf')
+        best_move = None
+        for move in get_all_moves(position, WHITE, game):
+            evaluation = minimax(move, depth-1, False, game)[0]
+            max_eval = max(max_eval, evaluation)
+            if max_eval == evaluation:
+                best_move = move
+        return max_eval, best_move
+    else:
+        min_eval = float('inf')
+        best_move = None
+        for move in get_all_moves(position, RED, game):
+            evaluation = minimax(move, depth - 1, True, game)[0]
+            min_eval = min(min_eval, evaluation)
+            if min_eval == evaluation:
+                best_move = move
+        return min_eval, best_move
+
+
+def simulate_move(piece, move, board, game, skip):
+    board.move(piece, move[0], move[1])
+    if skip:
+        board.remove(skip)
+    return board
+
+
+def get_all_moves(board, color, game):
+    moves = []
+
+    for piece in board.get_all_pieces(color):
+        valid_moves = board.get_valid_moves(piece)
+        for move, skip in valid_moves.items():
+            temp_board = deepcopy(board)
+            temp_piece = temp_board.get_piece(piece.row, piece.col)
+            new_board = simulate_move(temp_piece, move, temp_board, game, skip)
+            moves.append(new_board)
+
+    return moves
